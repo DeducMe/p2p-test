77d6e37f74b7f19d70ef22f136931980e0b0c19d
DeducMe <deducMe@gmail.com>
2021-04-25 22:48:52 +0300

    fix

--- front/index.js
+++ front/index.js
@@ -4,8 +4,8 @@ const FOOD_COLOR = '#e66916';
 const GRID_SIZE = 100;
 
 let SIZE;
-const socket = io('https://video-test-p2p.herokuapp.com/');
-// const socket = io('http://localhost:5000/');
+// const socket = io('https://video-test-p2p.herokuapp.com/');
+const socket = io('http://localhost:5000/');
 
 socket.on('init', handleSocketInit);
 socket.on('callState', handleCallState);
@@ -71,9 +71,19 @@ function init(){
     })
     .then(stream => {
         createStream(stream)
+        return
     })
     .catch(error => {
-        createStream(emptyMediaStream)
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        })
+        .then(stream => {
+            createStream(stream)
+            return
+        })
+        .catch(error => {
+            createStream(emptyMediaStream)
+        });
     });
     
 }
